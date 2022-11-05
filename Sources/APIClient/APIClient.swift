949a11cc33dbb011226fb9dfb2998833f113b7fc
Leonardo Chinigioli <scr037@Leonardos-MacBook-Pro.local>
2022-11-05 09:20:06 -0300

    Adding responseWithCompletion() method for scenarios that don't support concurrency

--- Sources/APIClient/APIClient.swift
+++ Sources/APIClient/APIClient.swift
@@ -26,7 +26,46 @@ extension APIClient {
     let output = try responseHandler.handleSuccess(data)
     return .success(output)
   }
+}
 
+// MARK: iOS 12 Compatibility
+
+extension APIClient {
+  public func responseWithCompletion<Output, Error>(
+    request: APIClientRequest,
+    responseHandler: APIClientResponseHandler<Output, Error>,
+    completion: @escaping (Result<Output, APIClientError> ) -> Void
+  ) where Error: Swift.Error {
+    let request = request.requestFrom(configuration: configuration)
+    session.dataTask(with: request) { data, response, error in
+      let httpResponse = response as! HTTPURLResponse
+      
+      guard let data = data else { return }
+      guard 200 ..< 300 ~= httpResponse.statusCode else {
+        do {
+          let error = data.isEmpty ? nil : try responseHandler.handleFailure(data)
+          let httpError = HTTPError(statusCode: httpResponse.statusCode, error: error)
+          completion(.failure(.httpError(httpError)))
+        } catch {
+          completion(.failure(.invalidResponse))
+        }
+        return
+      }
+
+      do {
+        let output = try responseHandler.handleSuccess(data)
+        completion(.success(output))
+      } catch {
+        completion(.failure(.invalidResponse))
+      }
+    }
+    .resume()
+  }
+}
+
+// MARK: Custom
+
+extension APIClient {
   public func responseHeaders(request: APIClientRequest) async throws -> [AnyHashable: Any]? {
     let request = request.requestFrom(configuration: configuration)
     let result = try await session.data(for:request)
