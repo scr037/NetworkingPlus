13e63b5d336666bcaa17896e4ecff73f3dbde23c
Leonardo Chinigioli <scr037@gmail.com>
2023-09-05 20:59:59 -0300

    Remove dependencies

--- Sources/APIClient/APIClient+Dependencies.swift
+++ Sources/APIClient/APIClient+Dependencies.swift
@@ -1,15 +1,15 @@
-import Dependencies
-
-public struct APIClientDependencyKey: TestDependencyKey {
-  public static var testValue: APIClient = .failing
-  public static var previewValue: APIClient = .mock(
-    url: "https://github.com/scr037/NetworkingPlus"
-  )
-}
-
-extension DependencyValues {
-  public var apiClient: APIClient {
-    get { self[APIClientDependencyKey.self] }
-    set { self[APIClientDependencyKey.self] = newValue }
-  }
-}
+//import Dependencies
+//
+//public struct APIClientDependencyKey: TestDependencyKey {
+//  public static var testValue: APIClient = .failing
+//  public static var previewValue: APIClient = .mock(
+//    url: "https://github.com/scr037/NetworkingPlus"
+//  )
+//}
+//
+//extension DependencyValues {
+//  public var apiClient: APIClient {
+//    get { self[APIClientDependencyKey.self] }
+//    set { self[APIClientDependencyKey.self] = newValue }
+//  }
+//}
